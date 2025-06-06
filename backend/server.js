9898b06ddbf7accc58715c3d1079bf87b9eab770
yash <yashmine926@gmail.com>
2025-06-06 13:23:44 +0530

    Update server.js with latest configuration

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const authRoute = require("./routes/authRoute");
+const authRoute = require("./routes/authroute.js");
 require("dotenv").config();
 
 const app = express();
